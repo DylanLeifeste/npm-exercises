f1976feacc1b5601e6b9f83e5370827817429e7a
Dylan Leifeste <ldylan01@gmail.com>
2020-05-26 16:28:21 -0500

    npm

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const $ = require('jquery');
+
+
+ let sayHello = () => {
+    console.log('Hello')
+};
+
+$(document).ready(alert('san fransisco wheres your disco?? bwwahhhhh'));
+
+sayHello();
