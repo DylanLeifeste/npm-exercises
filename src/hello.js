98b5a1878cdcad4cffe44bc67f1b0a09c3d712ec
Dylan Leifeste <ldylan01@gmail.com>
2020-05-27 16:52:05 -0500

    modules

---
+++ src/hello.js
@@ -0,0 +1,5 @@
+
+
+export const nice = () => "i'm your huckleberry";
+
+// module.exports = nice();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 "use strict";
+import {nice} from './hello';
+
 const $ = require('jquery');
+
 
 
  let sayHello = () => {
     console.log('Hello')
 };
 
-$(document).ready(alert('san fransisco wheres your disco?? bwwahhhhh'));
+ $(document).ready(alert('san fransisco wheres your disco?? bwwaaahhhh'));
 
 sayHello();
+
+
+
+console.log(nice);
